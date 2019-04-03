f40f95e20a89e2ada4e5998dee9f8147a30634a8
James Pier <jpier2012@gmail.com>
2019-04-03 13:48:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,5 @@
 # Write your code here.
 
-require 'pry'
-
 def badge_maker name
   puts "Hello, my name is #{name}."
 end
@@ -14,8 +12,7 @@ def assign_rooms names
     names.each_with_index {|name, index| puts "Hello, #{name}! You'll be assigned to room #{index}!"}
 end
 
-binding.pry
-
-#def printer names
-
-#end
+def printer names
+  batch_badge_creator names
+  assign_rooms names
+end
