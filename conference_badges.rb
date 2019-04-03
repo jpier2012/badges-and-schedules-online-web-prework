b39e07fe717dc4b0898ee0b8ae91291924e44042
James Pier <jpier2012@gmail.com>
2019-04-03 13:57:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 require 'pry'
 
-names = *[]
+badges = *[]
 
 def badge_maker name
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator names
-  names.each {|name| puts badge_maker name}
+  names.each {|name| badges.push badge_maker name}
 end
 
 def assign_rooms names
