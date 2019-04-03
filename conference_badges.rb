30439b814afcd450b2da445df3ef19f9f39d55a8
James Pier <jpier2012@gmail.com>
2019-04-03 14:23:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 def assign_rooms names
     rooms = *[]
     msg = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-    names.each_with_index {|name, index| 
+    names.each_with_index {|name, index|
       rooms.push(msg)
       puts msg
     }
