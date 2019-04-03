6c5a4991cba30c2866457c85eacdbb9ed31f776d
James Pier <jpier2012@gmail.com>
2019-04-03 14:05:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,15 @@
 require 'pry'
 
 name_list = *["James", "Allen", "Pier"]
-badges = *[]
 
 def badge_maker name
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator names
+  badges = *[]
   names.each {|name| badges.push(badge_maker(name))}
+  badges
 end
 
 def assign_rooms names
